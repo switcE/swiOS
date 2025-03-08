5eb5ab30f176022417778d089f69135bc5619b9b
switcE <moha.nofal778@gmail.com>
2025-03-08 19:27:51 +0300

    Update bootloader.h

--- include/bootloader.h
+++ include/bootloader.h
@@ -1,6 +1,6 @@
-#ifndef BOOTLOADER_H
-#define BOOTLOADER_H
+    #ifndef BOOTLOADER_H
+    #define BOOTLOADER_H
 
-// Declarations for bootloader functions (if needed)
+    // Declarations for bootloader functions (if needed)
 
-#endif
+    #endif
